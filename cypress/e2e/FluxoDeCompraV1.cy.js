f2cbc92c77d78a16e7596e447f6b99fcb3f99632
Gabriel de Oliveira <gabriel.oliveira_012@hotmail.com>
2025-02-11 23:21:24 -0300

    swaglabsV1_update

---
+++ cypress/e2e/FluxoDeCompraV1.cy.js
@@ -0,0 +1,22 @@
+describe("template spec", () => {
+  it("Login Inválido", () => {
+    cy.visit("https://www.saucedemo.com/v1/index.html");
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("senha");
+    cy.get("#login-button").click();
+
+    cy.get('[data-test="error"]').contains(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+  it("Login válido", () => {
+    cy.visit("https://www.saucedemo.com/v1/index.html");
+
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+
+    cy.get("#login-button").click();
+
+    cy.get(".product_label").contains("Products");
+  });
+});
